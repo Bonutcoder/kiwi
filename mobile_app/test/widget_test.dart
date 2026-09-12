3a539eb255e427462996398838dd6e098edd9f9f
Anurag <bhetsy.ka.dost@gmail.com>
2026-09-12 14:11:27 +0530

    Remove Flutter application codebase, keeping UI/UX designs

--- mobile_app/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:mobile_app/main.dart';
-
-void main() {
-  testWidgets('KiwiApp loads dashboard screen test', (WidgetTester tester) async {
-    await tester.pumpWidget(const KiwiApp());
-    expect(find.textContaining('secure your'), findsOneWidget);
-  });
-}
